683dd266de44bded1ca138145a90c7bf3d05419c
Steve <Steve@DESKTOP-JDB4ENH>
2021-06-25 12:28:17 -0400

    Agregar archivos de proyecto.

---
+++ PPacmanUSFX/Fruta.cpp
@@ -0,0 +1,88 @@
+#include "Fruta.h"
+#include <iostream>
+
+Fruta::Fruta(Tile* _tile, Texture* _frutaTexture) :
+	GameActor(_frutaTexture)
+{
+	tileActual = _tile;
+
+	if (tileActual != nullptr)
+		tileActual->setFruta(this);
+
+	tipoFruta = TIPO_FRUTA_FRUTILLA;
+
+	visible = false;
+
+}
+
+Fruta::~Fruta() {
+	deleteGameObject();
+}
+
+void Fruta::reconfigurar(Tile* _tileNuevo, TIPO_FRUTA _tipoFruta) {
+	if (tileActual != nullptr) {
+		tileActual->setFruta(nullptr);
+	}
+
+	tileActual = _tileNuevo;
+
+	if (tileActual != nullptr) {
+		tileActual->setFruta(this); {
+			posicionX = tileActual->getPosicionX() * Tile::anchoTile;
+			posicionY = tileActual->getPosicionY() * Tile::altoTile;
+		}
+		colisionador = new SDL_Rect({ posicionX, posicionY, 0, 0 });
+	}
+	switch (_tipoFruta)
+	{
+	case TIPO_FRUTA_GUINDA:
+		frameY = 0; break;
+	case TIPO_FRUTA_PLATANO:
+		frameY = 1; break;
+	case TIPO_FRUTA_NARANJA:
+		frameY = 2; break;
+	case TIPO_FRUTA_FRUTILLA:
+		frameY = 3; break;
+	}
+}
+
+void Fruta::setTile(Tile* _tileNuevo) {
+
+	if (tileActual != nullptr) {
+		tileActual->setFruta(nullptr);
+	}
+
+	tileActual = _tileNuevo;
+
+	if (tileActual != nullptr) {
+		tileActual->setFruta(this); {
+			posicionX = tileActual->getPosicionX() * Tile::anchoTile;
+			posicionY = tileActual->getPosicionY() * Tile::altoTile;
+			ancho = Tile::anchoTile;
+			alto = Tile::altoTile;
+		}
+		colisionador = new SDL_Rect({ posicionX, posicionY, 0, 0 });
+	}
+}
+
+void Fruta::update()
+{
+	if (cont >= timeFruit) {
+		cont = 0;
+		deleteGameObject();
+	}
+	else
+		cont++;
+}
+
+void Fruta::deleteGameObject() {
+	GameObject::deleteGameObject();
+	tileActual->setFruta(nullptr);
+}
+
+void Fruta::render()
+{
+	SDL_Rect rect = { posicionX, posicionY, ancho, alto };
+	SDL_Rect clip = { 0 + frameX * anchoClip, 0 + frameY * altoClip, anchoClip, altoClip };
+	textura->render(posicionX, posicionY, &clip, &rect);
+};
---
+++ PPacmanUSFX/Fruta.h
@@ -0,0 +1,61 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <SDL.h>
+
+#include "GameActor.h"
+#include "Texture.h"
+#include "TileGraph.h"
+#include "Tile.h"
+
+using namespace std;
+enum TIPO_FRUTA {
+	TIPO_FRUTA_GUINDA,
+	TIPO_FRUTA_PLATANO,
+	TIPO_FRUTA_NARANJA,
+	TIPO_FRUTA_FRUTILLA,
+	MAX
+};
+
+enum GameFrutaType {
+	FRUTA_CLASICO,
+	FRUTA_GALACTICO,
+	FRUTA_ASESINO
+};
+
+class Fruta : public GameActor {
+protected:
+
+	TIPO_FRUTA tipoFruta;
+	GameFrutaType GameFrutaTipo;
+
+	const int timeFruit = 400;
+	int cont = 0;
+
+	int frameX = 0;
+	int frameY;
+	int altoClip = 25;
+	int anchoClip = 25;
+
+	Tile* tileActual;
+
+public:
+
+	Fruta(Tile* _tile, Texture* _frutaTexture);
+	~Fruta();
+
+	void reconfigurar(Tile* _tileNuevo, TIPO_FRUTA tipoFruta);
+
+	virtual Fruta* clone() = 0;
+
+	//Metodos accesores
+	TIPO_FRUTA getTipoFruta() { return tipoFruta; }
+	GameFrutaType getTipo() { return GameFrutaTipo; }
+
+	void setTile(Tile* _tileNuevo);
+
+	void update();
+	void render() override;
+
+	void deleteGameObject() override;
+};
